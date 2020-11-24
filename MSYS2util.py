e2dd3a4ae5df464c71ffac601914e2381dd68f0a
bs2kbs2k <bs2k@naver.com>
2020-11-24 13:42:48 +0900

    msys2util ftw

--- MSYS2util.py
+++ MSYS2util.py
@@ -1,7 +1,13 @@
 from subprocess import run as srun
+import subprocess
 import shlex
 
 
-def run(working_dir, command, cmd_input=None):
-    return srun(["cmd", "/c", "set MSYSTEM=MINGW64&&" + "MSYS2/msys2/usr/bin/bash.exe" + " --login -c " +
-                shlex.quote("cd {} && {}".format(working_dir, command))], capture_output=True, input=cmd_input)
+def run(command, working_dir='', cmd_input=None):
+    return srun(["cmd", "/c", "set MSYSTEM=MINGW64&& MSYS2/msys2/usr/bin/bash.exe --login -c " +
+                 shlex.quote("cd {} && {}".format(working_dir, command))],
+                capture_output=True, input=cmd_input, stderr=subprocess.STDOUT, text=True).stdout
+
+
+def r(log, command, working_dir='', cmd_input=None):
+    print(run(command, working_dir, cmd_input), file=log)
--- backend.py
+++ backend.py
@@ -5,6 +5,7 @@ import dlmgr
 import filelib
 import safetar
 from shellinteract import InteractiveShell
+import MSYS2util
 
 
 class Builder:
@@ -27,41 +28,24 @@ class Builder:
 
 
 class MSYS2Builder(Builder):
-    URL = "https://github.com/msys2/msys2-installer/releases/download/nightly-x86_64/msys2-base-x86_64-latest.tar.xz"
 
     def __init__(self, build_type, build_dir, log_file):
         super().__init__(build_type, build_dir, log_file)
 
     def prepare(self, reused=None):
-        file = dlmgr.download(MSYS2Builder.URL, self.build_dir)
         log_file = open(self.log_file, 'a')
-        with tarfile.open(file) as f:
-            f.extractall(self.build_dir, members=safetar.safemembers(f, log_file))
-        print('Running MSYS2 first startup...', file=log_file)
-        shell = InteractiveShell(["cmd", "/c", "set MSYSTEM=MINGW64&& set PS1=shell_prompt$&& {} --login"
-                                 .format(join(self.build_dir, normpath('msys2/usr/bin/bash.exe')))],
-                                 'shell_prompt$', log_file)
-        shell.stop()
-        print('Updating MSYS2...', file=log_file)
-        shell = InteractiveShell(["cmd", "/c", "set MSYSTEM=MINGW64&& set PS1=shell_prompt$&& {} --login"
-                                 .format(join(self.build_dir, normpath('msys2/usr/bin/bash.exe')))],
-                                 'shell_prompt$', log_file)
-        shell.execute_and_get_output('pacman -Syu')
         print('Installing dependencies...', file=log_file)
-        shell.execute_and_get_output('pacman -S --noconfirm {}'.format(self.build_type['deps']['MSYS2']))
+        MSYS2util.r(log_file, 'pacman -Syu --noconfirm --needed {}'.format(self.build_type['deps']['MSYS2']))
         print('Cloning the repository...', file=log_file)
-        shell.execute_and_get_output('cd {}'.format(filelib.win_to_msys(self.build_dir)))
-        shell.execute_and_get_output(
-            'git clone {} {} portrepo'.format(self.build_type['cloneopts'], self.build_type['repo']))
-        shell.execute_and_get_output('cd portrepo')
+        MSYS2util.r(log_file,
+                    'git clone {} {} portrepo'.format(self.build_type['cloneopts'],
+                                                      self.build_type['repo']), self.build_dir)
         if reused:
-            filelib.cp_list(reused, filelib.win_to_msys(self.build_dir) + '/portrepo/')
-            shell.stop()
+            filelib.cp_list(reused, join(self.build_dir, 'portrepo'))
             return reused
         print('Preparing...', file=log_file)
-        shell.execute_and_get_output(self.build_type["prepare_cmd"])
-        ret = shell.execute_and_get_output(self.build_type["list_reusable"])
-        shell.stop()
+        MSYS2util.r(log_file, self.build_type["prepare_cmd"], join(self.build_dir, 'portrepo'))
+        ret = MSYS2util.r(log_file, self.build_type["list_reusable"], join(self.build_dir, 'portrepo'))
         return ret.split('\n')[:-1]
 
     def apply_patches(self, patches):
